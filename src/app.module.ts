ca1c6871e87476860f64fb01fe9f358e4cf216e0
dan-mutua <danmutua15@gmail.com>
2024-06-26 19:18:28 +0300

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+   // ThrottlerModule.forRoot({ limit: 10, ttl: 60 }),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+
+    // feature module
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
