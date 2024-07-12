f3750609d90bfdd49b6076e2ae31d30c02a910c1
dan-mutua <danmutua15@gmail.com>
2024-07-12 14:42:12 +0300

    update gitignore

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './model/user.schema';
 
 @Module({
-  controllers: [UserController],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
